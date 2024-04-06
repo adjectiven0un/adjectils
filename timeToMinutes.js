83ae95776671d065f606f376298726518af518f0
adjective_noun <adjectivenoun3215@gmail.com>
2024-04-05 22:12:36 -0400

    Fix a timer issue at 60 minutes

--- timeToMinutes.js
+++ timeToMinutes.js
@@ -7,7 +7,7 @@ function timeToMinutes(seconds){
     if (second < 10){
         second = "0" + second;
     }
-    if (minutes > 60){
+    if (minutes >= 60){
         var hours = Math.floor(minutes / 60);
         minutes -= hours * 60;
         if (minutes < 10){
