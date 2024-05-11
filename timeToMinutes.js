cdb66b93f8035aef0f88e9c53b6531a932139a21
adjective_noun <adjectivenoun3215@gmail.com>
2024-05-10 23:58:49 -0400

    Added event timers to home page

--- timeToMinutes.js
+++ timeToMinutes.js
@@ -1,21 +1,22 @@
 function timeToMinutes(seconds){
     var minutes = Math.floor(seconds/60);
     var second = seconds % 60;
-    if (minutes < 10){
-        minutes = "0" + minutes;
-    }
-    if (second < 10){
-        second = "0" + second;
-    }
+    minutes = fixTime(minutes);
+    second = fixTime(second);
     if (minutes >= 60){
         var hours = Math.floor(minutes / 60);
+        hours = fixTime(hours);
         minutes -= hours * 60;
-        if (minutes < 10){
-            minutes = "0" + minutes;
+        minutes = fixTime(minutes);
+        if (hours >= 24){
+            var days = Math.floor(hours / 24);
+            hours -= days * 24;
+            hours = fixTime(hours);
+            return (" " + days + ":" + hours + ":" + minutes + ":" + fixTime(Math.floor(second)));
         }
-        return(hours + ":" + minutes + ":" + second);
+        return(" " + hours + ":" + minutes + ":" + fixTime(Math.floor(second)));
     }
-    return (minutes + ":" + second);
+    return (" " + minutes + ":" + fixTime(Math.floor(second)));
 }
 function fixTime(unit){
     if (unit < 10){
