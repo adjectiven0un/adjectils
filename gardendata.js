d08b5f2b6be61000cba749e6b6848c5d211744ac
adjective_noun <adjectivenoun3215@gmail.com>
2024-08-11 18:56:01 -0400

    Added basic composter stuff

--- gardendata.js
+++ gardendata.js
@@ -131,6 +131,227 @@ const crops = {
         milestones: [90, 150, 250, 500, 1000, 2000, 4000, 7500, 10000, 15000, 20000, 25000, 30000, 50000, 100000, 150000, 200000, 300000, 500000, 750000, 1000000, 1500000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000, 9000000],
     },
 }
+var upgrades = {
+    "speed": 
+    {
+        "title": "Composter Speed",
+        "items": ['a', 'b', 'a', 'b', 'a', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c', 'b', 'c'],
+        "counts": [16, 2, 32, 4, 64, 8, 1, 16, 2, 32, 3, 48, 4, 64, 6, 96, 8, 128, 10, 192, 13, 256, 16, 320, 20],
+        "a": {
+            "name": "Enchanted Hay Bale",
+            "cost": 7776,
+        },
+        "b": {
+            "name": "Enchanted Golden Carrot",
+            "cost": 61440,
+        },
+        "c": {
+            "name": "Tightly-Tied Hay Bale",
+            "cost": 1119744,  
+        },
+    },
+    "multi_drop": 
+    {
+        "title": "Multi-drop",
+        "items": ['a', 'b', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a', 'c', 'a'],
+        "counts": [1, 64, 2, 1, 4, 2, 8, 4, 16, 8, 32, 16, 48, 32, 64, 48, 96, 64, 128, 96, 192, 128, 256, 192, 320],
+        "a": {
+            "name": "Enchanted Baked Potato",
+            "cost": 76800,
+        },
+        "b": {
+            "name": "Enchanted Pumpkin",
+            "cost": 1600,
+        },
+        "c": {
+            "name": "Polished Pumpkin",
+            "cost": 256000,  
+        },
+    },
+    "fuel_cap": 
+    {
+        "title": "Fuel Cap",
+        "items": ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a'],
+        "counts": [1, 4, 2, 8, 4, 16, 8, 32, 16, 48, 32, 64, 48, 96, 64, 128, 96, 192, 128, 256, 192, 384, 256, 512, 320],
+        "a": {
+            "name": "Enchanted Sugar Cane",
+            "cost": 102400,
+        },
+        "b": {
+            "name": "Enchanted Melon Block",
+            "cost": 51200,
+        },
+
+    },
+    "organic_matter_cap": 
+    {
+        "title": "Organic Matter Cap",
+        "items": ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a'],
+        "counts": [1, 3, 2, 6, 4, 12, 7, 24, 10, 48, 16, 72, 24, 96, 32, 144, 48, 192, 64, 256, 96, 352, 128, 448, 160],
+        "a": {
+            "name": "Enchanted Cactus",
+            "cost": 76800,
+        },
+        "b": {
+            "name": "Enchanted Cookie",
+            "cost": 61500,
+        },
+    },
+    "cost_reduction": 
+    {
+        "title": "Cost Reduction",
+        "items": ['a', 'b', 'c', 'b', 'd', 'b', 'c', 'b', 'd', 'b', 'c', 'b', 'd', 'b', 'c', 'b', 'd', 'b', 'c', 'b', 'd', 'b', 'c', 'b', 'd'],
+        "counts": [32, 1, 2, 2, 4, 4, 16, 8, 32, 16, 64, 32, 128, 48, 256, 64, 448, 96, 640, 160, 832, 224, 1024, 288, 1216],
+        "a": {
+            "name": "Enchanted Brown Mushroom",
+            "cost": 1600,
+        },
+        "b": {
+            "name": "Mutant Nether Wart",
+            "cost": 102400,
+        },
+        "c": {
+            "name": "Enchanted Red Mushroom Block",
+            "cost": 51200,
+        },
+        "d": {
+            "name": "Enchanted Brown Mushroom Block",
+            "cost": 51200,
+        },
+    },
+
+}
+var upgradesuniversal = {
+    1: {
+        copper: 100,
+    },
+    2: {
+        copper: 150,
+    },
+    3: {
+        copper: 200,
+    },
+    4: {
+        copper: 250,
+    },
+    5: {
+        copper: 300,
+    },
+    6: {
+        copper: 350,
+    },
+    7: {
+        copper: 400,
+    },
+    8: {
+        copper: 500,
+        crop: "Cropie",
+        count: 3,
+        cost: "25000",
+    },
+    9: {
+        copper: 600,
+        crop: "Cropie",
+        count: 6,
+        cost: "25000",
+    },
+    10: {
+        copper: 700,
+        crop: "Cropie",
+        count: 12,
+        cost: "25000",
+    },
+    11: {
+        copper: 800,
+        crop: "Cropie",
+        count: 32,
+        cost: "25000",
+    },
+    12: {
+        copper: 900,
+        crop: "Cropie",
+        count: 64,
+        cost: "25000",
+    },
+    13: {
+        copper: 1000,
+        crop: "Cropie",
+        count: 128,
+        cost: "25000",
+    },
+    14: {
+        copper: 1200,
+        crop: "Squash",
+        count: 3,
+        cost: "75000",
+    },
+    15: {
+        copper: 1400,
+        crop: "Squash",
+        count: 6,
+        cost: "75000",
+    },
+    16: {
+        copper: 1600,
+        crop: "Squash",
+        count: 12,
+        cost: "75000",
+    },
+    17: {
+        copper: 1800,
+        crop: "Squash",
+        count: 32,
+        cost: "75000",
+    },
+    18: {
+        copper: 2000,
+        crop: "Squash",
+        count: 64,
+        cost: "75000",
+    },
+    19: {
+        copper: 2250,
+        crop: "Squash",
+        count: 128,
+        cost: "75000",
+    },
+    20: {
+        copper: 2500,
+        crop: "Fermento",
+        count: 3,
+        cost: "250000",
+    },
+    21: {
+        copper: 2750,
+        crop: "Fermento",
+        count: 6,
+        cost: "250000",
+    },
+    22: {
+        copper: 3000,
+        crop: "Fermento",
+        count: 12,
+        cost: "250000",
+    },
+    23: {
+        copper: 3300,
+        crop: "Condensed Fermento",
+        count: 4,
+        cost: "2250000",
+    },
+    24: {
+        copper: 3600,
+        crop: "Condensed Fermento",
+        count: 7,
+        cost: "2250000",
+    },
+    25: {
+        copper: 4000,
+        crop: "Condensed Fermento",
+        count: 14,
+        cost: "2250000",
+    },
+}
 
 function sum(array) {
     var total = 0;
